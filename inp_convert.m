c9c3a3469da6a2136b56bf38878ceb2cf96aa1de
Steven Becker <stbe@linuxlaptop>
2019-12-02 11:02:57 +0100

    inp_convert.m: input-Ordner als default-Ausgabeordner, und Aenderungen am GUI-Text.

--- inp_convert.m
+++ inp_convert.m
@@ -1,6 +1,9 @@
 function inp_convert()
+
+% Als Funktion implementiert, damit Variablen anschliessend geloescht werden.
+
 %START
-fprintf('P R AE P R O Z E S S O R   S C H N I T T S T E L L E\n\n');
+fprintf('\nP R AE P R O Z E S S O R   S C H N I T T S T E L L E\n');
 clearvars;
 
 %Material
@@ -21,73 +24,76 @@ start2=false;
 
 eline=inf;
 e2line=inf;
-Emod=-inf;                  
-nu=inf;                      
+Emod=-inf;
+nu=inf;
 
 %INPUT-/OUTPUT-ORNDER
 
 %DATEI IM INPUTDATEI FESTLEGEN
 %Dateiname und Ordnerpfad speichern
-[Inputdatei,inpOrdner]=uigetfile('*.inp','Inputdatei im Abaqus-Format fuer DAEDALON festlegen');
+[Inputdatei,inpOrdner]=uigetfile('*.inp','Inputdatei im Abaqus-Format fuer DAEdalon festlegen');
 
 %OUTPUT ORDNER FESTLEGE
 %Ordnerpfad speichern
-outOrdner=uigetdir('','Outputordner fuer DAEdalon-*.inp festlegen');
+outOrdner=uigetdir('input','Outputordner fuer DAEdalon-*.inp festlegen');
 
 %Abfrage, ob schon Dateien vorhanden sind
 if (isfile([outOrdner,filesep,'node.inp']) ==1 )
-    answer1 = questdlg('Im Ordner wurden schon Dateien gefunden. Diese überschreiben?', ...
+    answer1 = questdlg('Im Ordner wurden schon Dateien gefunden. Diese ueberschreiben?', ...
 	'WARNUNG', ...
-	'JA','NEIN','NEIN');
+	'Ja','Nein','Nein');
 
      switch answer1
-        case 'JA'
-            disp("UEberschreiben der Dateien");
-            
-        case 'NEIN'
-            disp("Alte Dateien werden behalten");
-            %Abbruch des Codes, wenn NEIN gewaehlt wird
+        case 'Ja'
+            disp("Vorhandene Dateien werden ueberschrieben.");
+
+        case 'Nein'
+            disp("Alte Dateien werden behalten.");
+            %Abbruch des Codes, wenn 'Nein' gewaehlt wird
             return;
      end
-     
-     %Falls Eingabefenster geschlossen wird, Dateien überschreiben
+
+     %Falls Eingabefenster geschlossen wird, Dateien ueberschreiben
      if (isempty(answer1)==1)
-         uiwait(msgbox('Fenster wurde geschlossen. Alte Dateien werden ueberschrieben','WARNUNG')); %warten bis aktion an Fenster durchgeführt wird
-         
+         uiwait(msgbox('Fenster wurde geschlossen. Alte Dateien werden ueberschrieben.','WARNUNG')); %warten bis aktion an Fenster durchgefuehrt wird
      end
-     
+
 end
 
-%ABFRAGE EMODUL & NÜ : Solange bis Werte für Emodul & Poissonszahl i.O (z.B:Poissonzahl max. 0.5)
+%ABFRAGE Emodul & nu : Solange bis Werte für Emodul & Poissonszahl i.O.
 while Emod<=Emodmin || nu>numax || nu<numin
-  
+
+  prompttxt1 = 'E-Modul E > 0 (Einheiten beachten! Bsp.: mm -> MPa, m -> Pa)';
+  prompttxt2 = 'Querkontraktionszahl nu=[-1, 0.5]';
+  promptwarn = 'Eingabe ausserhalb des Definitionsbereichs! ';
+
   if Emod==-inf  && nu==inf
-    prompt = {'Gebe den E-Modul ein (Einheiten beachten Bsp.: mm -> MPa, m -> Pa)[Wertebereich >0]','Gebe die Querkontraktionszahl (Poissonzahl) ein [Wertebereich -1 bis 0.5]'};
+    prompt = {prompttxt1, prompttxt2};
     dlgtitle = 'INPUT';
     definput = {'210000','0.3'};
   else
-   if Emod<=Emodmin && nu<numax && nu>numin
-    prompt = {'E-Modul außerhalb des Wertebereichs: Gebe den E-Modul erneut ein (Einheiten beachten Bsp.: mm -> MPa, m -> Pa) [Wertebereich >0]','Gebe die Querkontraktionszahl (Poissonzahl) ein [Wertebereich -1 bis 0.5]'};
-    dlgtitle = 'INPUT';
-    definput = {'210000',num2str(nu)};
-   else
-    if Emod>Emodmin && nu>numax || Emod>Emodmin && nu<numin
-     prompt = {'Gebe den E-Modul ein (Einheiten beachten Bsp.: mm -> MPa, m -> Pa) [Wertebereich >0]','Querkontraktionszahl außerhalb des Wertebereichs: Gebe die Querkontraktionszahl (Poissonzahl) erneut ein [Wertebereich -1 bis 0.5]'};
-     dlgtitle = 'INPUT';
-     definput = {num2str(Emod),'0.3'};
+    if Emod<=Emodmin && nu<numax && nu>numin
+      prompt = {strcat(promptwarn,prompttxt1), prompttxt2};
+      dlgtitle = 'INPUT';
+      definput = {'210000',num2str(nu)};
     else
-     prompt = {'E-Modul außerhalb des Wertebereichs: Gebe den E-Modul erneut ein (Einheiten beachten Bsp.: mm -> MPa, m -> Pa) [Wertebereich >0]','Querkontraktionszahl außerhalb des Wertebereichs: Gebe die Querkontraktionszahl (Poissonzahl) erneut ein [Wertebereich -1 bis 0.5]'};
-     dlgtitle = 'INPUT';
-     definput = {'210000','0.3'};
+      if Emod>Emodmin && nu>numax || Emod>Emodmin && nu<numin
+        prompt = {strcat(promptwarn,prompttxt1), prompttxt2};
+        dlgtitle = 'INPUT';
+        definput = {num2str(Emod),'0.3'};
+      else
+        prompt = {strcat(promptwarn,prompttxt1), prompttxt2};
+        dlgtitle = 'INPUT';
+        definput = {'210000','0.3'};
+      end
     end
-   end
   end
   dlgtitle = 'INPUT';
   answer2 = inputdlg(prompt,dlgtitle,[1 40],definput);
   Emod = str2num(answer2{1});
   nu = str2num(answer2{2});
 
-end
+end % while
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %EXTRAHIEREN DER 'node' UND 'el' DATEI
@@ -108,33 +114,31 @@ fid_mat = fopen( pfad_mat, 'w' );
 fid = fopen(pfad);
 
 %EXTRAHIEREN START
-a = {};  
-tline = fgetl(fid); 
+a = {};
+tline = fgetl(fid);
 
-while ischar(tline) 
-    
+while ischar(tline)
+
     %Zeilenweise auslesen
-    tline = fgetl(fid); 
+    tline = fgetl(fid);
     a{length(a)+1,1} = tline;
-    
+
     %Extrahieren der 'node'
-    
-    %Suchen nach dem Start-Schlagwort 
-    if(isempty(strfind(tline,'*Node'))==0)  
+
+    %Suchen nach dem Start-Schlagwort
+    if(isempty(strfind(tline,'*Node'))==0)
         sline=length(a)+1;
         start=true;
     end
-    
-    
-    
+
     %Umschreiben des CellArrays 'a' in ein neues gekuerztes Cellarray 'node'
     if (start==true)
         %Suchen nach dem Pause-Schlagwort (Leere Zeile)
-        if((isempty(tline)==1)) 
-         eline=length(a)-4;   
+        if((isempty(tline)==1))
+         eline=length(a)-4;
         end
-        
-        %Extrahierungsvorgang von Start-Zeile (sline) 
+
+        %Extrahierungsvorgang von Start-Zeile (sline)
         %bis Pause-Zeile (eline)
         if((length(a)>=sline)&&(length(a)<=(eline)))
             count=count+1;
@@ -143,31 +147,28 @@ while ischar(tline)
             %Voranngestellte Durchnummerierung in erster Spalte des Strings im Array löschen (z.B 45,46,47,48...)
             empt=strfind(m_node{count,1},' ');
             %Engueltige Nodes rausschreiben in Cell-Array
-            node{count,1}=m_node{count,1}((empt(1)+1):end) ;         
+            node{count,1}=m_node{count,1}((empt(1)+1):end) ;
         end
-    end
-    
+    end % start
+
     %Exrahieren der 'el'
-    
-    %Suchen nach dem Start-Schlagwort 
-    if(isempty(strfind(tline,'*Element'))==0)  
+
+    %Suchen nach dem Start-Schlagwort
+    if(isempty(strfind(tline,'*Element'))==0)
         s2line=length(a)+1;
         start2=true;
         set=true;
         count=0;
     end
-    
-    
-    
+
     %Umschreiben des CellArrays in eine Matrix 'm_node'
     if (start2==true)
-        
+
             %Suchen nach einer leeren Zeile
             if((isempty(tline)==1))
                 e2line=length(a)-2;
             end
-        
-        
+
         if((length(a)>=s2line)&&length(a)<=(e2line))
             count=count+1;
             %Kuerzen des Cell-Arrays a und Kommata loeschen
@@ -175,12 +176,11 @@ while ischar(tline)
             %Voranngestellte Durchnummerierung in erster Spalte des Strings im Array löschen (z.B 45,46,47,48...)
             empt=strfind( m_el{count,1},' ');
             %Engueltige Nodes rausschreiben in Cell-Array
-            el{count,1}=[num2str(material),' ',m_el{count,1}((empt(1)+1):end)];   
-            
+            el{count,1}=[num2str(material),' ',m_el{count,1}((empt(1)+1):end)];
         end
-    end
-    
-end
+    end % start2
+
+end % while
 
 zn=length(node);
 ze=length(el);
@@ -201,7 +201,6 @@ fclose(fid_el);
 fprintf('Erzeugt: %s\n',pfad_el);
 
 %geom.inp
-
 fprintf(fid_geom, '%g\t', [zn]); %Zeile 1: Anzahl Zeilen aus node.inp
 fprintf(fid_geom, '\n'); %Zeilenumbruch
 fprintf(fid_geom, '%g\t', [ze]); %Zeile 2: Anzahl Zeilen aus el.inp
@@ -214,9 +213,9 @@ fprintf(fid_geom, '%g\t', [sn]); %Zeile 5: Anzahl der Freiehitsgrade pro Knoten 
 fprintf(fid_geom, '\n');
 fprintf(fid_geom, '%g\t', [se1]); %Zeile 6: Anzahl Knoten pro Element (Anzahl Spalten aus el.inp-1)
 fclose (fid_geom);
+fprintf('Erzeugt: %s\n',pfad_geom);
 
 %mat1.inp
-
 fprintf(fid_mat, '%g\t', [5]); %Zeile 1: Fester Wert (Elementtyp)
 fprintf(fid_mat, '\n'); %Zeilenumbruch
 fprintf(fid_mat, '%g\t', [se1]); %Zeile 2: Anzahl an Integrationspunkten (Zeile 6 aus geom inp.)
@@ -229,21 +228,6 @@ fprintf(fid_mat, '%g\t', [Emod]); %Zeile 5: E-Modul in MPa
 fprintf(fid_mat, '\n');
 fprintf(fid_mat, '%g\t', [nu]); %Zeile 6: Querkontraktionszahl (Poissonzahl)
 fclose (fid_mat);
+fprintf('Erzeugt: %s\n',pfad_mat);
 
-
-fprintf('\n');
-fprintf('B E E N D E T - O R D N E R   P R U E F E N ');
-fprintf('\n');
-end
-
-
-
-
-
-
-
-
-
-
-
-
+end % function
