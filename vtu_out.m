303aac2890e9148f557fbf5f17090baddc11d958
Steven Becker <stbe@linuxlaptop>
2019-12-04 16:23:50 +0100

    vtu_out.m: Output jew. in eine Zeile schreiben.

--- vtu_out.m
+++ vtu_out.m
@@ -17,8 +17,8 @@ fprintf(fid,'%s%s%s%s%s\n','<Piece NumberOfPoints="',num2str(size(node,1)),'" Nu
 %Schreiben der Knotenkoordinaten
 fprintf(fid,'%s\n','<Points>');
 fprintf(fid,'%s%s%s\n','<DataArray type="Float64" Name="Points" NumberOfComponents="',num2str(size(node,2)),'" format="ascii">');
-fprintf(fid,'%f\n',node');
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%f ',node');
+fprintf(fid,'\n%s\n','</DataArray>');
 fprintf(fid,'%s\n','</Points>');
 
 %Innerhalb des PointData Feldes stehen alle Informationen zu den einzelnen
@@ -27,18 +27,18 @@ fprintf(fid,'%s\n','<PointData>');
 
 %Schreiben der Verschiebungen
 fprintf(fid,'%s%s%s\n','<DataArray type="Float64" Name="Disp" NumberOfComponents="',num2str(size(node,2)),'" format="ascii">');
-fprintf(fid,'%f\n',u);
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%f ',u);
+fprintf(fid,'\n%s\n','</DataArray>');
 
 %Schreiben der Dehnungen
-fprintf(fid,'%s%s%s\n','<DataArray type="Float64" Name="Dehnungen" NumberOfComponents="6" format="ascii">');
-fprintf(fid,'%f\n',cont_mat_node(:,1:6)');
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%s\n','<DataArray type="Float64" Name="Dehnungen" NumberOfComponents="6" format="ascii">');
+fprintf(fid,'%f ',cont_mat_node(:,1:6)');
+fprintf(fid,'\n%s\n','</DataArray>');
 
 %Schreiben der Spannungen
-fprintf(fid,'%s%s%s\n','<DataArray type="Float64" Name="Spannungen" NumberOfComponents="6" format="ascii">');
-fprintf(fid,'%f\n',cont_mat_node(:,7:12)');
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%s\n','<DataArray type="Float64" Name="Spannungen" NumberOfComponents="6" format="ascii">');
+fprintf(fid,'%f ',cont_mat_node(:,7:12)');
+fprintf(fid,'\n%s\n','</DataArray>');
 
 fprintf(fid,'%s\n','</PointData>');
 
@@ -50,20 +50,20 @@ fprintf(fid,'%s\n','<Cells>');
 % el2=el;
 % el2(:,[2,3])=el2(:,[3,2]);
 fprintf(fid,'%s\n','<DataArray type="Int64" Name="connectivity" format="ascii">');
-fprintf(fid,'%i\n',(el-1)');
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%i ',(el-1)');
+fprintf(fid,'\n%s\n','</DataArray>');
 
 %offsets
 offsets=4:4:4*size(el,1);
 fprintf(fid,'%s\n','<DataArray type="Int64" Name="offsets" format="ascii">');
-fprintf(fid,'%i\n',offsets');
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%i ',offsets');
+fprintf(fid,'\n%s\n','</DataArray>');
 
 %Typ der Elemente
 type=10*ones(size(el,1),1);
 fprintf(fid,'%s\n','<DataArray type="Int64" Name="types" format="ascii">');
-fprintf(fid,'%i\n',type);
-fprintf(fid,'%s\n','</DataArray>');
+fprintf(fid,'%i ',type);
+fprintf(fid,'\n%s\n','</DataArray>');
 
 fprintf(fid,'%s\n','</Cells>');
 fprintf(fid,'%s\n','</Piece>');
