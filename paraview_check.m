a5b10ec94f037a82ee6bb092562620bd7a474611
Steven Becker <stbe@linuxlaptop>
2019-12-06 13:59:51 +0100

    paraview_check.m: Windows-Pfad zu Paraview-Macros korrigiert.

--- paraview_check.m
+++ paraview_check.m
@@ -2,7 +2,7 @@
 
 % Ort des Konfig-Ordnes, je nach Betriebssystem
 if ispc
-    paraview_config_path = strcat(getenv('APPDATA'),'\ParaView');
+    paraview_config_path = strcat(getenv('APPDATA'),'\Roaming\ParaView');
 elseif isunix || ismac
     paraview_config_path = strcat(getenv('HOME'),'/.config/ParaView');
 end
