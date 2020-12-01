a3402f748f2e644e55c8bcc927a922c5f5c0eca1
Steven Becker <stbe@linuxlaptop>
2020-12-01 09:13:29 +0100

    defgrad_x.m: Allgemeinere Formulierung.

--- defgrad_x.m
+++ defgrad_x.m
@@ -34,6 +34,9 @@ function [F] = defgrad_x(u_elem,dshape)
 %   = d(x-u)/dx
 %   = I - du/dx
 
+dim = size(u_elem);
+dim = dim(2);             % 2- oder 3-D ?
+
 u_x = u_elem' * dshape;
-F = inv(-u_x + eye(2));
+F = inv(-u_x + eye(dim));
 
