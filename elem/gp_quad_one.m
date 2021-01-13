7e617d02c494e828a5b8ac5941bd25863757f01a
Steven Becker <stbe@linuxlaptop>
2021-01-13 15:52:33 +0100

    Add single-point Gauss routines (weights and points) for quad and brick elements.

---
+++ elem/gp_quad_one.m
@@ -0,0 +1,35 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                  %
+%    DAEdalon Finite-Element-Project                               %
+%                                                                  %
+%    Copyright 2020 Steven Becker                                  %
+%    Contact: http://www.daedalon.org                              %
+%                                                                  %
+%                                                                  %
+%    This file is part of DAEdalon.                                %
+%                                                                  %
+%    DAEdalon is free software; you can redistribute it            %
+%    and/or modify it under the terms of the GNU General           %
+%    Public License as published by the Free Software Foundation;  %
+%    either version 2 of the License, or (at your option)          %
+%    any later version.                                            %
+%                                                                  %
+%    DAEdalon is distributed in the hope that it will be           %
+%    useful, but WITHOUT ANY WARRANTY; without even the            %
+%    implied warranty of MERCHANTABILITY or FITNESS FOR A          %
+%    PARTICULAR PURPOSE.  See the GNU General Public License       %
+%    for more details.                                             %
+%                                                                  %
+%    You should have received a copy of the GNU General            %
+%    Public License along with Foobar; if not, write to the        %
+%    Free Software Foundation, Inc., 59 Temple Place, Suite 330,   %
+%    Boston, MA  02111-1307  USA                                   %
+%                                                                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Rückgabe der nötigen GP an denen das Integral ausgewertet wird
+
+function [gpcoor, gpweight]= gp_quad_one
+
+gpcoor=[0.0, 0.0];
+gpweight=[4.0];
