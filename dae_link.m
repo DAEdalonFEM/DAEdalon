24654513eea691625a83653b7f55bf719bbbb017
Steven Becker <stbe@linuxlaptop>
2021-11-03 09:26:16 +0100

    dae_link.m: Use intrinsic 'web' function to open link in browser.

--- dae_link.m
+++ dae_link.m
@@ -1,14 +1,7 @@
 function dae_link
 
-sys = computer;
-if isunix
-    try
-        unix('xdg-open http://www.daedalon.org')
-    catch
-        disp('Browser konnte nicht geoeffnet werden!')
-    end
-end
-
-if strncmp(sys, 'PC', 2)
-    disp('Funktion nicht implementiert!')
+try
+    web('http://daedalon.org')
+catch
+    disp('Browser konnte nicht geoeffnet werden!')
 end
